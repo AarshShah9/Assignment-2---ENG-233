d1be30df4934be9fa17c2b5e4db5aa3416cca89a
AarshShah9 <aarshshah99@gmail.com>
2021-10-14 16:19:40 -0600

    Added to encode function

--- encryption.py
+++ encryption.py
@@ -5,84 +5,64 @@
 # Remember to include docstrings for your functions and comments throughout your code.
 
 ### Define your functions here
-
-
-# def characters(letter):
-#     alphabet = {
-#         'a': letter[0],
-#         'b': letter[1],
-#         'c': letter[2],
-#         'd': letter[3],
-#         'e': letter[4],
-#         'f': letter[5],
-#         'g': letter[6],
-#         'h': letter[7],
-#         'i': letter[8],
-#         'j': letter[9],
-#         'k': letter[10],
-#         'l': letter[11],
-#         'm': letter[12],
-#         'n': letter[13],
-#         'o': letter[14],
-#         'p': letter[15],
-#         'q': letter[16],
-#         'r': letter[17],
-#         's': letter[18],
-#         't': letter[19],
-#         'u': letter[20],
-#     }
-
-#     numbers = {}
+import string
 
 
 def encode():
     """The encode function takes a message and encodes it into a """
-    user_message = input('Please enter the text to be processed: ').lower()
+    alphabet = string.ascii_lowercase
+    numbers = string.digits
+
+    message = input('Please enter the text to be processed: ').lower()
     cipher_text = input(
         'Please enter the cipher text (alpha-numerical only): ').lower()
-    cipher_check1 = cipher_text.isalnum(
-    )  # Might have to switch and get it to check if the cipher text exists within a dictionary or list
-    if cipher_check1 == True:
-        cipher_text_nodupl = ''.join(dict.fromkeys(cipher_text))
-    else:
-        print('The provided cipher is not alpha-numerical please try again')
-        encode()
+    s = list(cipher_text)
+    alphanumeric = {
+        'a': cipher_text[0],
+        'b': cipher_text[1],
+        'c': cipher_text[2],
+        'd': cipher_text[3],
+        'e': cipher_text[4],
+        'f': cipher_text[5],
+        'g': cipher_text[6],
+        'h': cipher_text[7],
+        'i': cipher_text[8],
+        'j': cipher_text[9],
+        'k': cipher_text[10],
+        'l': cipher_text[11],
+        'm': cipher_text[12],
+        'n': cipher_text[13],
+        'o': cipher_text[14],
+        'p': cipher_text[15],
+        'q': cipher_text[16],
+        'r': cipher_text[17],
+        's': cipher_text[18],
+        't': cipher_text[19],
+        'u': cipher_text[20],
+        'v': cipher_text[21],
+        'w': cipher_text[22],
+        'x': cipher_text[23],
+        'y': cipher_text[24],
+        'z': cipher_text[25]
+    }
 
-    if cipher_text_nodupl.len() == 26:
-        print('Your cipher is valid.')
-        encoded_message = ''
-        print(f'Your output is: {encoded_message}')
+    if cipher_text.isalnum(
+    ) == True:  # Might have to switch and get it to check if the cipher text exists within a dictionary or list
+        cipher_text_nodupl = ''.join(dict.fromkeys(cipher_text))
+        if len(cipher_text_nodupl) == 26:
+            print('Your cipher is valid.')
+        else:
+            print(
+                'The provided cipher is not alpha-numerical please try again')
+            encode()
     else:
         print('The provided cipher is not 26 characters please try again')
         encode()
-    
-    letter = cipher_text_nodupl
-    alphabet = {
-        'test': 'a',
-        'b': letter[1],
-        'c': letter[2],
-        'd': letter[3],
-        'e': letter[4],
-        'f': letter[5],
-        'g': letter[6],
-        'h': letter[7],
-        'i': letter[8],
-        'j': letter[9],
-        'k': letter[10],
-        'l': letter[11],
-        'm': letter[12],
-        'n': letter[13],
-        'o': letter[14],
-        'p': letter[15],
-        'q': letter[16],
-        'r': letter[17],
-        's': letter[18],
-        't': letter[19],
-        'u': letter[20],
-    }
-    
-    for letters, value in alphabet:
-        message1 = 
+
+    for key, value in alphanumeric.items():
+        # Replace key character with value character in string
+        final_message = message.replace(key, value)
+        print(final_message)
 
     main_menu()
 
